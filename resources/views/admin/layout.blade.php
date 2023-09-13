2d738616e317508970aace6b8fb22ffb20750091
Karim Hossam <140264221+KarimHossam362002@users.noreply.github.com>
2023-09-13 06:19:33 +0300

    dashboard style and selling prepare

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -122,7 +122,7 @@
             <div class="icon">
               <i class="ion ion-stats-bars"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="{{ route('sellings.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
