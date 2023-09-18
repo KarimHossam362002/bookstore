8ab16ecc5577dc28fbb7514a88847c091884ad12
Karim Hossam <140264221+KarimHossam362002@users.noreply.github.com>
2023-09-18 16:40:10 +0300

    Add files via upload

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cart;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    function index(){
+        $carts = Cart::select('id','total','status')->paginate(5);
+        return view('admin.cart.index' , compact('carts'));
+    }
+    function destroy(string $id){
+        Cart::where('id', $id)->delete();
+        return back()->with('success', 'Cart deleted successfully');
+    }
+}
